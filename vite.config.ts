a2eea394ca311f4f3ebfcc8806eb185f055b00b8
Micky <6802580+mickyf@users.noreply.github.com>
2026-01-17 11:07:54 +0100

    fix PWA not asking for update

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
 
     // PWA plugin
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'prompt',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'Celly - Wine Cellar Manager',
